c5d46407ec87b70f68c857ab5447c70b465ff8f2
CODY DE LA TORRE <cdelatorre414@gmail.com>
2022-11-15 15:04:19 -0800

    setup basic server

--- server.js
+++ server.js
@@ -1,16 +1,49 @@
 'use strict';
 
-const { application } = require('express');
-const { response } = require('express');
+const express = require('express');
 let data = require('./data/weather.json');
 
+// need to bring in the .env file
 require('dotenv').config();
 
 const cors = require('cors');
 
+//USE
 const app = express();
+
+//define the PORT
+const PORT = process.env.PORT || 3002;
 
 app.use(cors());
 
 
-application.listen(PORT, () => console.log(`listening on port ${PORT}`));
+app.get('/weather', (request, response) => {
+  let place = request.query.place;
+  let selectedPlace = data.find(loc => loc.city_name === place);
+  console.log(selectedPlace);
+  let weatherParsed = selectedPlace.data.map(day => new Forecast(day));
+  response.send(weatherParsed);
+});
+
+app.get('/', (request, response) => {
+  response.send('Hello from our server');
+});
+
+app.get('/sayHello', (request, response) => {
+  response.send(`Hi ${request.query.name}`);
+});
+
+app.get('*', (request, response) => {
+  response.send('That route does not exist homie');
+});
+
+
+class Forecast {
+  constructor(day) {
+    this.date = day.datetime;
+    this.description = day.weather.description;
+  }
+}
+
+//LISTEN
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
