e9efc632e14b0cef91abdd333f559992cf3654ac
CODY DE LA TORRE <cdelatorre414@gmail.com>
2022-11-18 17:20:50 -0800

    finished lab10

---
+++ MYCODE/myServer.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const express = require('express');
+
+// need to bring in the .env file
+require('dotenv').config();
+
+const cors = require('cors');
+
+//USE
+const app = express();
+
+app.use(cors());
+
+const getWeather = require('./myWeather.js');
+const getMovies = require('../modules/movies.js');
+
+//define the PORT
+const PORT = process.env.PORT || 3002;
+
+//ROUTES
+
+app.get('/', (request, response) => {
+  response.send('Hello from our server');
+});
+
+app.get('/weather', getWeather);
+
+
+app.get('/movies', getMovies);
+
+
+app.get('*', (request, response) => {
+  response.send('That route does not exist homie');
+});
+
+//handle errors
+app.use((error, request, response, next) => {
+  response.status(500).send(error.message);
+});
+
+//LISTEN
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
--- server.js
+++ server.js
@@ -1,43 +1,17 @@
 'use strict';
 
-const express = require('express');
-
-// need to bring in the .env file
 require('dotenv').config();
-
+const express = require('express');
 const cors = require('cors');
-
-//USE
+const weatherHandler = require('./modules/weather.js');
+const getMovies = require('./modules/movies.js');
 const app = express();
-
-app.use(cors());
-
-const getWeather = require('./weather.js');
-const getMovies = require('./movies.js');
-
-//define the PORT
 const PORT = process.env.PORT || 3002;
 
-//ROUTES
 
-app.get('/', (request, response) => {
-  response.send('Hello from our server');
-});
-
-app.get('/weather', getWeather);
-
-
+app.use(cors());
+app.get('/weather', weatherHandler);
 app.get('/movies', getMovies);
 
 
-app.get('*', (request, response) => {
-  response.send('That route does not exist homie');
-});
-
-//handle errors
-app.use((error, request, response, next) => {
-  response.status(500).send(error.message);
-});
-
-//LISTEN
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server up on ${PORT}`));
